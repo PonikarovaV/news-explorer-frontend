3cb1b6667f1c1f2bf7b6662ba8038ec0f6be4944
Поникарова Ванда Евгеньевна <ponikarova.ve@esphere.ru>
2020-07-02 08:43:33 +0300

    feat: blocks & styles

--- src/pages/articles/articles.js
+++ src/pages/articles/articles.js
@@ -1,3 +1,13 @@
 import './articles.css';
 
-console.log('Hello atricles');
+const openMenuIcon = document.querySelector('.header__menu-icon');
+const closeMenuIcon = document.querySelector('.header__menu-close-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+openMenuIcon.addEventListener('click', () => {
+  mobileMenu.classList.remove('mobile-menu_invisible');
+});
+
+closeMenuIcon.addEventListener('click', () => {
+  mobileMenu.classList.add('mobile-menu_invisible');
+});
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,14 +1,13 @@
 import './index.css';
 
-const menuIcon = document.querySelector('.header__menu-icon');
+const openMenuIcon = document.querySelector('.header__menu-icon');
 const closeMenuIcon = document.querySelector('.header__menu-close-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuIcon.addEventListener('click', () => {
-  closeMenuIcon.classList.add('header__menu-close-icon_visible');
-  menuIcon.classList.add('header__menu-icon_invisible');
+openMenuIcon.addEventListener('click', () => {
+  mobileMenu.classList.remove('mobile-menu_invisible');
 });
 
 closeMenuIcon.addEventListener('click', () => {
-  closeMenuIcon.classList.remove('header__menu-close-icon_visible');
-  menuIcon.classList.remove('header__menu-icon_invisible');
+  mobileMenu.classList.add('mobile-menu_invisible');
 });
